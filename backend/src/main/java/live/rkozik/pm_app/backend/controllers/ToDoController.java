832daaf39d584630946fae4fa35e9e97fbf58d43
Robert Kozik <rkozik98@gmail.com>
2021-03-23 20:24:07 +0100

    Create test controller object

--- backend/src/main/java/live/rkozik/pm_app/backend/controllers/ToDoController.java
+++ backend/src/main/java/live/rkozik/pm_app/backend/controllers/ToDoController.java
@@ -1,2 +1,24 @@
-package live.rkozik.pm_app.backend.controllers;public class ToDoController {
+package live.rkozik.pm_app.backend.controllers;
+
+import live.rkozik.pm_app.backend.repositories.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoController {
+
+    final private ToDoRepository repository;
+
+    @Autowired
+    public ToDoController(ToDoRepository _repository) {
+        this.repository = _repository;
+    }
+
+    @GetMapping("/ToDo")
+    public String getAllToDos() {
+        repository.findAll();
+        return "Hello World";
+    }
+
 }
