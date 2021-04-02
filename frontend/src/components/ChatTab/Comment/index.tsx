8285dec159e8f7c14b73fdc36117e1c18c2de619
Robert Kozik <rkozik98@gmail.com>
2021-04-02 19:41:30 +0200

    create single Comment component

---
+++ frontend/src/components/ChatTab/Comment/index.tsx
@@ -0,0 +1,26 @@
+import { MDBCol, MDBRow } from "mdb-react-ui-kit";
+import { Fragment } from "react";
+
+
+interface props {
+    own?: boolean|undefined,
+    content: string,
+    name: string
+}
+
+const Comment = ({own, content, name}: props) => {
+
+    return (
+        <Fragment>
+            <MDBRow className="mb-2" end={own?true:false}>
+                <MDBCol size="8" className={own?"bg-info text-end shadow rounded": "shadow text-start rounded"} >
+                    <p className="mb-0">{content}</p>
+                    {!own && <p className="text-muted mb-0"><small>{name}</small></p>}
+                </MDBCol>
+            </MDBRow>
+        </Fragment>
+    )
+
+}
+
+export default Comment;
