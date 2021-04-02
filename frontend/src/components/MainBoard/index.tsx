5293a973998bccb6a135c6304a4fc9d9cf210265
Robert Kozik <rkozik98@gmail.com>
2021-04-03 01:05:48 +0200

    fix mobile layout

--- frontend/src/components/MainBoard/index.tsx
+++ frontend/src/components/MainBoard/index.tsx
@@ -4,19 +4,22 @@ import DonutChart from "../DonutChart";
 import MembersTab from "../MembersTab";
 import ToDoList from "../ToDoList";
 const MainBoard = () => {
+
+    const width = window.innerWidth;
+    console.log(width);
     return(
         <MDBRow className="h-100">
             <div className="col-lg-6 col-md-12 h-100 border-end border-start">
                 <MDBContainer fluid>KANBON</MDBContainer>
             </div>
             <div className="col-lg-6 col-md-12 h-100">
-                <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100 border-bottom"> <ToDoList /> </MDBCol>
-                    <MDBCol size="6" className="h-100 border-bottom"> <MembersTab />  </MDBCol>
+                <MDBRow className="component-wrapper my-2">
+                    <MDBCol size="md-6 12" className="component"> <ToDoList /> </MDBCol>
+                    <MDBCol size="md-6 12" className="component"> <MembersTab />  </MDBCol>
                 </MDBRow>
-                <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100"> <DonutChart /> </MDBCol>
-                    <MDBCol size="6" className="h-100"> <ChatTab /> </MDBCol>
+                <MDBRow className="component-wrapper my-2">
+                    <MDBCol size="md-6 12" className="component"> <DonutChart /> </MDBCol>
+                    <MDBCol size="md-6 12" className="component"> <ChatTab /> </MDBCol>
                 </MDBRow>
             </div>
         </MDBRow>
