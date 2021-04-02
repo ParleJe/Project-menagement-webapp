5c7b23ba0264c5ec907ac3bf320c717f6ea68e49
Robert Kozik <rkozik98@gmail.com>
2021-04-02 20:03:28 +0200

    change position of components in grid

--- frontend/src/components/MainBoard/index.tsx
+++ frontend/src/components/MainBoard/index.tsx
@@ -11,11 +11,11 @@ const MainBoard = () => {
             </div>
             <div className="col-lg-6 col-md-12 h-100">
                 <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100 border-bottom"> <DonutChart /> </MDBCol>
                     <MDBCol size="6" className="h-100 border-bottom"> <ToDoList /> </MDBCol>
+                    <MDBCol size="6" className="h-100 border-bottom"> <MembersTab />  </MDBCol>
                 </MDBRow>
                 <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100"> <MembersTab /> </MDBCol>
+                    <MDBCol size="6" className="h-100"> <DonutChart /> </MDBCol>
                     <MDBCol size="6" className="h-100"> <ChatTab /> </MDBCol>
                 </MDBRow>
             </div>
