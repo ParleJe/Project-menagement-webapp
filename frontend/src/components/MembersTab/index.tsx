0782ecee54a9bf868e27411907b3f5961de7d6ee
Robert Kozik <rkozik98@gmail.com>
2021-04-02 15:15:45 +0200

    create MembersTab component with mocked data/add h-80 css class

--- frontend/src/components/MembersTab/index.tsx
+++ frontend/src/components/MembersTab/index.tsx
@@ -1,56 +1,42 @@
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardText, MDBCardTitle, MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
-import React from "react";
+import {MDBBtn, MDBCol, MDBContainer, MDBRow, MDBInputGroup, MDBInputGroupElement } from "mdb-react-ui-kit";
+import React, { Fragment } from "react";
+import User from "../../helpers/classes/User";
 
 
 const MembersTab = () => {
 
+    const UserMock = [new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), new User(1, "name", "surname", "", [], [], []), 
+                    new User(1, "name", "surname", "", [], [], []),new User(1, "name", "surname", "", [], [], []), ]
+
     return (
-        <MDBContainer className="h-100 bg-warning" fluid>
-            <MDBRow className="bg-primary h-25 my-1"> 
-            <MDBCol size="4 h-100 bg-secondary px-0">
-                <img className="img-fluid h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
-            </MDBCol>
-            <MDBCol center size="8" className="bg-success align-items-center">
-                <h5>Name Surname</h5>
-                <small>CEO</small>
-            </MDBCol>
-            </MDBRow>
-
-
-            <MDBRow className="bg-primary h-25 my-1"> 
-            <MDBCol size="4 h-100 bg-secondary px-0">
-                <img className="img-fluid h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
-            </MDBCol>
-            <MDBCol center size="8" className="bg-success align-items-center">
-                <h5>Name Surname</h5>
-                <small>CEO</small>
-            </MDBCol>
-            </MDBRow>
-
-            
-            <MDBRow className="bg-primary h-25 my-1"> 
-            <MDBCol size="4 h-100 bg-secondary px-0">
-                <img className="img-fluid h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
-            </MDBCol>
-            <MDBCol center size="8" className="bg-success align-items-center">
-                <h5>Name Surname</h5>
-                <small>CEO</small>
-            </MDBCol>
-            </MDBRow>
-
-            
+        <Fragment>
+            <MDBInputGroup className="mb-2 shadow"> 
+                    <MDBInputGroupElement type='text' placeholder="find by name" />
+                    <MDBBtn outline color="success">add</MDBBtn>
+            </MDBInputGroup>  
+        <MDBContainer className="w-100 h-80 overflow-auto">
+            {UserMock.map((user:User, key:number) => {
+                return (
+                    <MDBRow between className="h-25 border"> 
+                        <MDBCol size="3 h-100 px-0">
+                            <img alt="placeholder" className="img-fluid h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
+                        </MDBCol>
+                        <MDBCol center size="8" className="align-items-center">
+                            <h5>Name Surname</h5>
+                            <small>CEO</small>
+                        </MDBCol>
+                    </MDBRow>
+                )
+            })}          
         </MDBContainer>
+        </Fragment>
     );
 };
 
 export default MembersTab;
-
-// eslint-disable-next-line no-lone-blocks
-{/* <MDBCardBody>
-                    <MDBCardTitle>Card title</MDBCardTitle>
-                    <MDBCardText>
-                        Some quick example text to build on the card title and make
-                        up the bulk of the card&apos;s content.
-                    </MDBCardText>
-                    <MDBBtn href="#">MDBBtn</MDBBtn>
-                </MDBCardBody> */}
