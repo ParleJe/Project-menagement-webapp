80b7abd1edfc6f64b7d534ab98050718e0ae371d
Robert Kozik <rkozik98@gmail.com>
2021-04-02 19:40:13 +0200

    mount chat component, remove debug background color

--- frontend/src/components/MainBoard/index.tsx
+++ frontend/src/components/MainBoard/index.tsx
@@ -1,21 +1,22 @@
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+import ChatTab from "../ChatTab";
 import DonutChart from "../DonutChart";
 import MembersTab from "../MembersTab";
 import ToDoList from "../ToDoList";
 const MainBoard = () => {
     return(
         <MDBRow className="h-100">
-            <div className="col-lg-6 col-md-12 h-100 bg-primary">
+            <div className="col-lg-6 col-md-12 h-100 border-end border-start">
                 <MDBContainer fluid>KANBON</MDBContainer>
             </div>
             <div className="col-lg-6 col-md-12 h-100">
                 <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100 border-bottom"> <DonutChart/> </MDBCol>
-                    <MDBCol size="6" className="h-100 border-bottom"> <ToDoList/> </MDBCol>
+                    <MDBCol size="6" className="h-100 border-bottom"> <DonutChart /> </MDBCol>
+                    <MDBCol size="6" className="h-100 border-bottom"> <ToDoList /> </MDBCol>
                 </MDBRow>
                 <MDBRow className="h-50 my-2">
-                    <MDBCol size="6" className="h-100"> <MembersTab/> </MDBCol>
-                    <MDBCol size="6" className="h-100 bg-danger">CHAT TAB</MDBCol>
+                    <MDBCol size="6" className="h-100"> <MembersTab /> </MDBCol>
+                    <MDBCol size="6" className="h-100"> <ChatTab /> </MDBCol>
                 </MDBRow>
             </div>
         </MDBRow>
