67237f6690aeb793d3ea3e63071b67c78c9c1a65
Robert Kozik <rkozik98@gmail.com>
2021-04-03 21:41:39 +0200

    mount KanBan component

--- frontend/src/components/MainBoard/index.tsx
+++ frontend/src/components/MainBoard/index.tsx
@@ -1,18 +1,18 @@
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import ChatTab from "../ChatTab";
 import DonutChart from "../DonutChart";
+import Kanbon from "../Kanbon";
 import MembersTab from "../MembersTab";
 import ToDoList from "../ToDoList";
+
 const MainBoard = () => {
 
-    const width = window.innerWidth;
-    console.log(width);
     return(
-        <MDBRow className="h-100">
-            <div className="col-lg-6 col-md-12 h-100 border-end border-start">
-                <MDBContainer fluid>KANBON</MDBContainer>
+        <MDBRow className="h-100 p-0">
+            <div className="col-lg-7 col-md-12 h-100 pe-0 m-0">
+                <MDBContainer fluid className="w-100 h-100 pe-0"> <Kanbon /> </MDBContainer>
             </div>
-            <div className="col-lg-6 col-md-12 h-100">
+            <div className="col-lg-5 col-md-12 h-100">
                 <MDBRow className="component-wrapper my-2">
                     <MDBCol size="md-6 12" className="component"> <ToDoList /> </MDBCol>
                     <MDBCol size="md-6 12" className="component"> <MembersTab />  </MDBCol>
