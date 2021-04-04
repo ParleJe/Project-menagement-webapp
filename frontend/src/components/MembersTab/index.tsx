3ed8f96c472a04a7ce5b7ba308cb4ff3da8debb6
Robert Kozik <rkozik98@gmail.com>
2021-04-04 15:44:48 +0200

    change text padding

--- frontend/src/components/MembersTab/index.tsx
+++ frontend/src/components/MembersTab/index.tsx
@@ -23,13 +23,13 @@ const MembersTab = () => {
         <MDBContainer className="w-100 h-80 overflow-auto shadow-sm">
             {UserMock.map((user:User, key:number) => {
                 return (
-                    <MDBRow between className="h-25 border"> 
-                        <MDBCol size="3 h-100 px-0">
-                            <img alt="placeholder" className="img-fluid h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
+                    <MDBRow between className=" border"> 
+                        <MDBCol size="3 px-0">
+                            <img alt="placeholder" className="img-fluid w-100 h-100" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" />
                         </MDBCol>
-                        <MDBCol center size="8" className="align-items-center">
-                            <h5>Name Surname</h5>
-                            <small>CEO</small>
+                        <MDBCol center size="8" className="justify-center h-100">
+                            <h6 className="m-0 mt-2">Name Surname</h6>
+                            <p className="m-0 mb-2"><small>CEO</small></p>
                         </MDBCol>
                     </MDBRow>
                 )
