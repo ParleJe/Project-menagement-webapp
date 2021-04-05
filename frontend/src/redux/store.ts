2ef40e90efd581be4fc87d13a721706671c49762
Robert Kozik <rkozik98@gmail.com>
2021-04-05 13:17:28 +0200

    create redux template files

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: {
+      projects: projectsReducer,
+      hugeTasks: hugeTasksReducer,
+      smallTasks: smallTasksReducer,
+      toDos: toDosReducer,
+    },
+  })
+
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
