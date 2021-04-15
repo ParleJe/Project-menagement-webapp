66b46b3e8d62194f76c3d5ec3ebc2f7209d5723b
Robert Kozik <rkozik98@gmail.com>
2021-04-15 16:46:51 +0200

    change API helper file names

--- frontend/src/helpers/API/projects.ts
+++ frontend/src/helpers/API/projects.ts
@@ -3,17 +3,32 @@ import Project from "../classes/Project";
 const API_URL:string = "https://localhost:8080/api/projects/";
 
 const fetchAll = ():Promise<any> => {
-    const promise = fetch (API_URL);
+    const promise = fetch (API_URL, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+    });
     return promise;
 }
 
 const add = (project:Project):Promise<any> => {
-    const promise = fetch(API_URL+"TODO");
+    const promise = fetch(API_URL+"TODO", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+    });
     return promise;
 }
 
 const remove = (id:number):Promise<any> => {
-    const promise = fetch (API_URL+id);
+    const promise = fetch (API_URL+id, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json'
+          },
+    });
     return promise;
 }
 
