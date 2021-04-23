4c7697081fa5f2bd881df86711a5eca7a8105071
Robert Kozik <rkozik98@gmail.com>
2021-04-23 21:59:59 +0200

    create huge tasks redux slice

--- frontend/src/redux/slices/HugeTasksSlice.ts
+++ frontend/src/redux/slices/HugeTasksSlice.ts
@@ -1,18 +1,19 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import HugeTask from "../../helpers/classes/HugeTask";
-import {fetchAll, fetchById, add, remove, update} from '../../helpers/API/HugeTask';
+import HugeTask from "../../helpers/responseInterfaces/HugeTask";
+import {fetchAll, add, remove, update} from '../../helpers/API/HugeTask';
 
 interface HugeTaskState {
     HugeTasks: HugeTask[],
     selected: number,
     loading: 'idle' | 'pending' | 'succeeded' | 'failed'
-}
+};
 
 const initialState: HugeTaskState = {
     HugeTasks: [],
     selected: -1,
     loading: "idle"
 };
+
 //____________________________________________
 const fetchHugeTasks = createAsyncThunk(
     'HugeTask/fetchAllByProject',
@@ -22,13 +23,29 @@ const fetchHugeTasks = createAsyncThunk(
     }
   )
 
-  const removeHugeTasks = createAsyncThunk(
+const addHugeTask = createAsyncThunk(
+    'HugeTask/add',
+    async(hugeTask: HugeTask) => {
+        const response = await add(hugeTask.id, hugeTask);
+        return await response.json;
+    }
+    )
+
+const removeHugeTasks = createAsyncThunk(
     'HugeTask/fetchAllByProject',
-    async (HugeTask: HugeTask) => {
-      const response = await remove(HugeTask.id, HugeTask.idProject);
+    async (hugeTask: HugeTask) => {
+      const response = await remove(hugeTask.idProject, hugeTask.id);
       return await response.json();
     }
-  )
+    )
+
+const updateHugeTask = createAsyncThunk(
+      'HugeTask/update',
+      async (hugeTask: HugeTask) => {
+        const response = await update(hugeTask.idProject, hugeTask.id);
+        return await response.json();
+      }
+    )
 
 
 //____________________________________________
@@ -42,16 +59,40 @@ export const HugeTasksSlice = createSlice({
 
     },
     extraReducers: (builder) => {
-      builder.addCase(fetchProjects.pending, (state, action) => {
-        state.loading = "pending";
-      })
-      
+       builder.addCase(fetchHugeTasks.pending, (state) => {
+           state.loading = 'pending';
+       })
+       .addCase(fetchHugeTasks.fulfilled, (state, action) => {
+           state.loading = 'succeeded';
+           console.log(action.payload);
+       })
+       .addCase(fetchHugeTasks.rejected, (state) => {
+           state.loading = 'failed';
+       })
+
+       .addCase(addHugeTask.pending, (state) => {
+           state.loading = 'pending';
+       })
+       .addCase(addHugeTask.fulfilled, (state, action) => {
+           state.loading = 'succeeded';
+           console.log(action.payload);
+           state.HugeTasks.push(action.payload);
+       })
+       .addCase(addHugeTask.rejected, (state) => {
+           state.loading = 'failed';
+       })
+
+       .addCase(removeHugeTasks.fulfilled, (state, action) => {
+           state.HugeTasks.filter(HT => HT.id !== action.payload);
+       })
+
+       .addCase(updateHugeTask.fulfilled, (state, action) => { //can be more optimised!!!! O(n) always
+           state.HugeTasks.filter(HT => HT.id !== action.payload.id);
+           state.HugeTasks.push(action.payload);
+       })
     }
   });
 
 export const {select} = HugeTasksSlice.actions;
-export {fetchProjects, addProject, removeProject};
-
-
-
-export default projectSlice.reducer;
+export {fetchHugeTasks, removeHugeTasks, updateHugeTask, addHugeTask};
+export default HugeTasksSlice.reducer;
