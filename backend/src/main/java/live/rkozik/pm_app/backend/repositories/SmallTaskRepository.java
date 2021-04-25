0222eedf8f89913baadca026df3b84d3f6663300
Robert Kozik <rkozik98@gmail.com>
2021-04-25 16:50:54 +0200

    remove unused entities with interreleted files

--- backend/src/main/java/live/rkozik/pm_app/backend/repositories/SmallTaskRepository.java
+++
@@ -1,8 +0,0 @@
-package live.rkozik.pm_app.backend.repositories;
-
-import live.rkozik.pm_app.backend.models.SmallTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SmallTaskRepository extends JpaRepository<SmallTask, Long> {
-}
