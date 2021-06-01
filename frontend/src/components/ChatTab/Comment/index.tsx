899563cbe2c78036930bbfa4aaf606a9509e6d21
Robert Kozik <rkozik98@gmail.com>
2021-06-01 12:41:37 +0200

    refactor ChatTab component

--- frontend/src/components/ChatTab/Comment/index.tsx
+++ frontend/src/components/ChatTab/Comment/index.tsx
@@ -2,18 +2,18 @@ import { MDBCol, MDBRow } from "mdb-react-ui-kit";
 import { Fragment } from "react";
 
 
-interface props {
-    own?: boolean|undefined,
+interface CommentProps {
+    own?: boolean | undefined,
     content: string,
     name: string
 }
 
-const Comment = ({own, content, name}: props) => {
+const Comment = ({ own, content, name }: CommentProps) => {
 
     return (
         <Fragment>
-            <MDBRow className="mb-2" end={own?true:false}>
-                <MDBCol size="8" className={own?"bg-info text-end shadow rounded": "shadow text-start rounded"} >
+            <MDBRow className="mb-2" end={own ? true : false}>
+                <MDBCol size="8" className={own ? "bg-info text-end shadow rounded" : "shadow text-start rounded"} >
                     <p className="mb-0">{content}</p>
                     {!own && <p className="text-muted mb-0"><small>{name}</small></p>}
                 </MDBCol>
--- frontend/src/components/ChatTab/index.tsx
+++ frontend/src/components/ChatTab/index.tsx
@@ -1,28 +1,28 @@
 import { MDBBtn, MDBContainer, MDBInputGroup, MDBInputGroupElement } from "mdb-react-ui-kit";
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import Comment from "./Comment";
 
 const ChatTab = () => {
 
     const mockData = //[]
-    [{content: "bla bla bla", post:"Mike"},{content: "blah blah blah", post:"Sarah"}, 
-     {content: "blah blah blah", post:"Sarah"}, {content: "bla bla bla", post:"Mike"},
-     {content: "blah blah blah blah blah blah blah blah blah blah blah blah", post:"Sarah"}, {content: "bla bla bla", post:"Mike"},
-     {content: "bla bla bla", post:"Jeremy"},{content: "bla bla bla bla bla bla bla bla bla bla bla bla", post:"Jeremy"},]
+        [{ content: "bla bla bla", post: "Mike" }, { content: "blah blah blah", post: "Sarah" },
+        { content: "blah blah blah", post: "Sarah" }, { content: "bla bla bla", post: "Mike" },
+        { content: "blah blah blah blah blah blah blah blah blah blah blah blah", post: "Sarah" }, { content: "bla bla bla", post: "Mike" },
+        { content: "bla bla bla", post: "Jeremy" }, { content: "bla bla bla bla bla bla bla bla bla bla bla bla", post: "Jeremy" },]
 
     return (
         <Fragment>
             <MDBContainer fluid className="h-80 mb-2 overflow-auto shadow-sm">
-                {mockData.map((comment:any, key:number) => {
+                {mockData.map((comment: any, key: number) => {
                     return (
                         <Comment key={key} own={comment.post === "Mike"} name={comment.post} content={comment.content} />
                     )
                 })}
             </MDBContainer>
-            <MDBInputGroup className="shadow"> 
-                    <MDBInputGroupElement type='text' placeholder="what you think" />
-                    <MDBBtn outline color="secondary">post</MDBBtn>
-            </MDBInputGroup>  
+            <MDBInputGroup className="shadow">
+                <MDBInputGroupElement type='text' placeholder="what you think" />
+                <MDBBtn outline color="secondary">post</MDBBtn>
+            </MDBInputGroup>
         </Fragment>
     )
 
