39efbd72bda62ee160aaf496cd2034d0bd2a53c5
Robert Kozik <rkozik98@gmail.com>
2021-06-01 19:48:14 +0200

    add MapStruct config file to supress warnings

---
+++ backend/src/main/java/live/rkozik/pm_app/backend/configs/IgnoreUnmappedMapperConfig.java
@@ -0,0 +1,8 @@
+package live.rkozik.pm_app.backend.configs;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IgnoreUnmappedMapperConfig {
+}
